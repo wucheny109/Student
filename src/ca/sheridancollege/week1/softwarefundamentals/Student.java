c5f40c45d5955da6b0be4c039f6b558e24489e6c
wucheny109 <wucheny@sheridancollege.ca>
2020-01-21 17:17:28 -0500

    4rd commit

--- src/ca/sheridancollege/week1/softwarefundamentals/Student.java
+++ src/ca/sheridancollege/week1/softwarefundamentals/Student.java
@@ -47,6 +47,20 @@ public class Student {
         this.studentID = studentID;
     }
 
+    /**
+     * @return the status
+     */
+    public String getStatus() {
+        return status;
+    }
+
+    /**
+     * @param status the status to set
+     */
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
 }
  
    
